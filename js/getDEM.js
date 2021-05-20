c9c0df4caedc7c4485dfea975928add7ef184fc4
w-ockham <minoru.tomobe@gmail.com>
2021-05-20 18:41:39 +0900

    Change GSI DEM format from plain text to PNG.

---
+++ js/getDEM.js
@@ -0,0 +1,29 @@
+function getDEM(coords, done) { 
+    var url_png = "https://cyberjapandata.gsi.go.jp/xyz/dem_png/{z}/{x}/{y}.png";
+
+    var img = new Image();
+
+    img.crossOrigin = 'anonymous';
+    img.onload = function() {
+	var canvas = document.createElement('canvas'),
+	    context = canvas.getContext('2d'),
+	    dem = new Array(256*256);
+	canvas.width = 256;
+	canvas.height = 256;
+	context.drawImage(this, 0, 0);
+	d = context.getImageData(0,0,256,256).data;
+	for (var i = 0; i<= 0xffff; i++) {
+	    r = d[i*4 + 0];
+	    g = d[i*4 + 1];
+	    b = d[i*4 + 2];
+	    x = r * Math.pow(2,16) + g * Math.pow(2,8) +  b;
+	    h = ( x < Math.pow(2, 23))? x : x - Math.pow(2, 24);
+	    if ( h == -Math.pow(2,23))
+		h = 0;
+	    dem[i] = 0.01 * h;
+	}
+	if (done) done(dem)
+    }
+    url = L.Util.template(url_png, coords);
+    img.src = url;
+}
