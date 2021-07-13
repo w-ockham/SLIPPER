6946344ffadaa4c1a0263c021cf77a56f3a5c726
w-ockham <minoru.tomobe@gmail.com>
2021-07-13 23:53:42 +0900

    Import whole summits list from sotdata.org and local JA summits list.

--- summit_data/importdb.sh
+++ summit_data/importdb.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/bin:/bin
-rm tmp.csv tmpdb.db *-new
+TARGET=/home/ubuntu/sotaapp/backend/database
+cd $TARGET
+rm -f summitslist.csv tmp.csv tmpdb.db
+wget --quiet https://www.sotadata.org.uk/summitslist.csv
 sed -e '1,1d' summitslist.csv > tmp.csv
 sqlite3 tmpdb.db -separator , ".import tmp.csv allsummits"
-sqlite3 tmpdb.db ".read continent.txt"
-./make-summitlist.py
+sed -e '1i code,lat,lng,point,alt,name,region,name_k,region_k' ja_summits.csv > tmp.csv
+sqlite3 tmpdb.db -separator , ".import tmp.csv ja_summits" 
+date > import.log
+./make-summitlist.py tmpdb.db continent.csv &>> import.log
--- summit_data/make-summitlist.py
+++ summit_data/make-summitlist.py
@@ -1,55 +1,66 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
+import csv
 from datetime import datetime, timedelta
 import sqlite3
 import sys
 import re
-from sotakeys import *
 
-association_db = KEYS['ASSOC_DB']+"-new"
-dxsummit_db = KEYS['DXSUMMIT_DB']+"-new"
+dxsummit_db = 'summits.db'
 
-conn_tmp = sqlite3.connect("tmpdb.db")
-cur_tmp = conn_tmp.cursor()
-cur2_tmp = conn_tmp.cursor()
-conn_assoc = sqlite3.connect(association_db)
-cur_assoc = conn_assoc.cursor()
-conn_summit = sqlite3.connect(dxsummit_db)
-cur_summit = conn_summit.cursor()
+def read_dict(file):
+    with open(file, newline = "") as f:
+        read_dict = csv.reader(f, delimiter=",", quotechar='"')
+        return_dict = {}
 
-cur_assoc.execute("create table associations(code text,association text,continent text,primary key(code))")
-cur_summit.execute("CREATE TABLE summits (code txt,lat real,lng real,point integer,alt integer,name text,desc text)")
-cur_summit.execute("CREATE INDEX summit_index on summits(lat,lng)")
-cur_summit.execute("CREATE INDEX summit_code_idx on summits(code)")
+        for row in read_dict:
+            return_dict[row[0]] = row[2]
 
-conn_assoc.commit()
-conn_summit.commit()
-i = 0
-for s in cur_tmp.execute("select * from allsummits"):
-    (code,assoc,region,sname,altm,altf,gr1,gr2,lng,lat,pt,bp,validfrom,validto,_,_,_)= s
+    return return_dict
+
+def import_db(summitdb,continent):
+    conn_tmp = sqlite3.connect(summitdb)
+    cur_tmp = conn_tmp.cursor()
+    conn_summit = sqlite3.connect(dxsummit_db)
+    cur_summit = conn_summit.cursor()
+    cur_summit.execute("CREATE TABLE IF NOT EXISTS summits (code txt,lat real,lng real,point integer,bonus integer,alt integer,name text,name_k text,region text,region_k text,assoc text,continent text,actcount integer,actdate text,actcall text)")
+    cur_summit.execute("CREATE INDEX IF NOT EXISTS summit_index on summits(lat,lng)")
+    cur_summit.execute("CREATE INDEX IF NOT EXISTS summit_code_idx on summits(code)")
+    cur_summit.execute("delete from summits")
+    conn_summit.commit()
+
+    ctable = read_dict(continent)
+    i = 0
     now = int(datetime.utcnow().strftime("%s"))
-    valid = int(datetime.strptime(validto,'%d/%m/%Y').strftime("%s"))
-    if now < valid:
-        m = re.match('(\w+)/(\w+)',code)
-        prefix = m.group(1)
-        cur2_tmp.execute("select * from continent where prefix = '%s'" % prefix)
-        c = cur2_tmp.fetchone()
-        if c :
-            (_,_,continent) = c
-            q = "insert or replace into associations(code, association,continent) values(?, ?, ?)"
-            cur_assoc.execute(q,(code,assoc,continent))
-            q = "insert into summits(code,lat,lng,point,alt,name,desc) values(?, ?, ?, ?, ?, ?, ?)"
-            cur_summit.execute(q,(code,lat,lng,pt,altm,sname,region))
-            print "Import:" + code
-            i+=1
-        else:
-            print "Unknown association:" + prefix + " SummitCode=" + code
-            exit
-    else:
-        print "Obsolete summit:" + code + " skipped."
+    for s in cur_tmp.execute("select * from allsummits"):
+        (code,assoc,region,sname,altm,altf,gr1,gr2,lng,lat,pt,bp,validfrom,validto,actcount,actdate,actcall)= s
+        valid = int(datetime.strptime(validto,'%d/%m/%Y').strftime("%s"))
+        if now < valid:
+            try:
+                m = re.match('(\w+)/(\w+)',code)
+                continent = ctable[m.group(1)]
+                q = "insert or replace into summits(code,lat,lng,point,bonus,alt,name,name_k,region,region_k,assoc,continent,actcount,actdate,actcall) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+                cur_summit.execute(q,(code,lat,lng,pt,bp,altm,sname,sname,region,region,assoc,continent,actcount,actdate,actcall))
+                i+=1
+            except KeyError as e:
+                print("Association: '"+m.group(1)+"' not found")
 
-print str(i) + " summits has been imported."
-conn_assoc.commit()
-conn_summit.commit()
-conn_assoc.close()
-conn_summit.close()
+    print(str(i) + " summits has been imported.")
+
+    i = 0
+    for s in cur_tmp.execute("select * from ja_summits"):
+        (code,_,_,_,_,name,region,name_k,region_k) = s
+        q = "update summits set name = ?, name_k = ?, region = ?, region_k = ? where code = ?"
+        cur_summit.execute(q,(name,name_k,region,region_k,code))
+        i+=1
+    print(str(i) + " summits has been updated for JA association.")
+
+    conn_summit.commit()
+    conn_summit.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        import_db(sys.argv[1], sys.argv[2])
+    else:
+        print("Usage make_summitlist temp_db contient.csv")
+        
