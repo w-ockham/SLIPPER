87342b018edcf1a5c82db40262d947b9fd44d41d
w-ockham <minoru.tomobe@gmail.com>
2022-05-05 08:02:51 +0900

    Add cross reference check between JA-ARM and master ARM.

--- summit_data/importdb.sh
+++ summit_data/importdb.sh
@@ -2,13 +2,20 @@
 PATH=/usr/local/sbin:/usr/bin:/bin
 TARGET=/home/ubuntu/sotaapp/backend/database
 cd $TARGET
-rm -f summitslist.csv tmp.csv tmpdb.db
-wget --quiet https://www.sotadata.org.uk/summitslist.csv
-sed -e '1,1d' summitslist.csv > tmp.csv
-sqlite3 tmpdb.db -separator , ".import tmp.csv allsummits"
-sed -e '1i code,lat,lng,point,alt,name,region,name_k,region_k' ja_summits.csv > tmp.csv
-sqlite3 tmpdb.db -separator , ".import tmp.csv ja_summits" 
 date > import.log
+rm -f summitslist.csv tmp.csv tmpdb.db
+wget --quiet https://www.sotadata.org.uk/summitslist.csv &>> import.log
+sed -e '1,1d' summitslist.csv > tmp.csv
+sqlite3 tmpdb.db -separator , ".import tmp.csv allsummits" &>> import.log
+
+cat ja_summits.csv | sed -e 's/^[^[:alnum:]]*//' -e 's/\r//g' -e '1i code,lat,lng,point,alt,name,region,name_k,region_k' > tmp.csv
+sqlite3 tmpdb.db -separator , ".import tmp.csv ja_summits"  &>> import.log
+
+cat jaarm.csv ja5arm.csv ja6arm.csv ja8arm.csv| sed -e 's/^[^[:alnum:]]*//' -e 's/\r//g' -e '1i code,name,alt1,alt2,lon,lat,validfrom,validto,score,prom,clon,clat,name_k' > tmp.csv
+sqlite3 tmpdb.db -separator ',' ".import tmp.csv jaarm_summits"  &>> import.log
+
 ./make-summitlist.py tmpdb.db continent.csv association-new.db summits-new.db &>> import.log
+
 mv association-new.db association.db
 mv summits-new.db summits.db
+touch $TARGET/../uwsgi.touch
--- summit_data/make-summitlist.py
+++ summit_data/make-summitlist.py
@@ -53,15 +53,72 @@ def import_db(tmpdb, continent, assoc_db, summit_db):
             except KeyError as e:
                 print("Association: '"+m.group(1)+"' not found")
 
-    print(str(i) + " summits has been imported.")
+    print(str(i) + " summits has been imported from SOTADB.")
 
+    # JA-ARM Cross Check
+    ja,ja5,ja6,ja8 =(0,0,0,0)
+    for s in cur_tmp.execute("select * from jaarm_summits"):
+        (code,name,_,_,_,_,validfrom,validto,_,_,_,_,_) = s
+        if validto == '':
+            validto = '31/12/2099'
+        valid = int(datetime.strptime(validto,'%d/%m/%Y').strftime("%s"))
+        if now < valid:
+            cur_summit.execute(f"select count(*) from summits where code='{code}'")
+            j = cur_summit.fetchall()
+            if j[0][0] == 0:
+                print(f"JA-ARM {code} not found in SOTADB.")
+            if 'JA8/' in code:
+                ja8 +=1
+            elif 'JA6/' in code:
+                ja6 +=1
+            elif 'JA5/' in code:
+                ja5 += 1
+            else:
+                ja += 1
+    print(f"\nJA = {ja} summits, JA5 = {ja5} summits")
+    print(f"JA6 = {ja6} summits, JA8 = {ja8} summits\n")
+
+    ja,ja5,ja6,ja8 =(0,0,0,0)
+    for s in cur_summit.execute("select * from summits where code like 'JA%'"):
+        (code,_,_,_,_,_,_,_,_,_,_,_,_,_,_) = s
+        cur_tmp.execute(f"select count(*) from jaarm_summits where code='{code}'")
+        j = cur_tmp.fetchall()
+        if j[0][0] == 0:
+            print(f"SOTADB {code} not found in JA-ARM.")
+        if 'JA8/' in code:
+            ja8 +=1
+        elif 'JA6/' in code:
+            ja6 +=1
+        elif 'JA5/' in code:
+            ja5 += 1
+        else:
+            ja += 1
+    print(f"\nJA = {ja} summits, JA5 = {ja5} summits")
+    print(f"JA6 = {ja6} summits, JA8 = {ja8} summits\n")
+    
+    # Merge CTC's JCC/JCG data 
     i = 0
+    ja,ja5,ja6,ja8 =(0,0,0,0)
     for s in cur_tmp.execute("select * from ja_summits"):
         (code,lat,lng,_,_,name,region,name_k,region_k) = s
         q = "update summits set lat = ?, lng = ?, name = ?, name_k = ?, region = ?, region_k = ? where code = ?"
         cur_summit.execute(q,(lat,lng,name,name_k,region,region_k,code))
-        i+=1
+        if 'JA8/' in code:
+            ja8 +=1
+        elif 'JA6/' in code:
+            ja6 +=1
+        elif 'JA5/' in code:
+            ja5 += 1
+        else:
+            ja += 1
+        for j in cur_summit.execute("select changes()"):
+            if j[0] == 0:
+                print(f"CTC-DB {code} not found in SOTADB.")
+            else:
+                i+=1
     print(str(i) + " summits has been updated for JA association.")
+    print(f"\nJA = {ja} summits, JA5 = {ja5} summits")
+    print(f"JA6 = {ja6} summits, JA8 = {ja8} summits\n")
 
     conn_summit.commit()
     conn_summit.close()
@@ -73,4 +130,3 @@ if __name__ == "__main__":
         import_db(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
     else:
         print("Usage make_summitlist temp_db contient.csv association_db summit_db")
-        
